36868b884814673fdcd3f1b4973fefaaee50f725
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-01-20 00:49:46 +0900

    Update and rename Ex_JDBC1.java to Ex_JDBC.java

--- Ex_JDBC.java
+++ Ex_JDBC.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Ex_JDBC1 {
+public class Ex_JDBC {
 
 	public static void main(String[] args) {
 
