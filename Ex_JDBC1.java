cb53e0ca99300515c7991abc4674978485335e49
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-01-20 00:48:29 +0900

    Update Ex_JDBC1.java

--- Ex_JDBC1.java
+++ Ex_JDBC1.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Ex01_JDBC {
+public class Ex_JDBC1 {
 
 	public static void main(String[] args) {
 
