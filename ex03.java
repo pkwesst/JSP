5d82bc4d7da2b0bf414d40c5537152252243fe3d
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-02-05 23:59:57 +0900

    Update ex03.java

--- ex03.java
+++ ex03.java
@@ -1,5 +1,3 @@
-//ex03.java
-
 package com.example;
 
 import java.io.IOException;
@@ -15,25 +13,39 @@ import javax.servlet.http.HttpServletResponse;
 public class ex03 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//1. 사용자가 입력한 값을 가져오기
-		String num1 = request.getParameter("num1"); 
-		String num2 = request.getParameter("num2");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		//2. 사용자가 입력한 값이 문자 타입이므로 정수 타입으로 변환
+		// 1. 사용자가 입력한 값을 가져오기
+		String num1 = request.getParameter("num1");
+		String num2 = request.getParameter("num2");
+		String cal = request.getParameter("cal");
+
+		// 2. 사용자가 입력한 값이 문자 타입이므로 정수 타입으로 변환
 		int num3 = Integer.parseInt(num1);
 		int num4 = Integer.parseInt(num2);
-				
-		System.out.println(num3+num4);
 
-		//3. 브라우저창에 출력
-	      response.setContentType("text/html;charset=utf-8");
-	      PrintWriter out = response.getWriter();
-	      
-	      out.println(num3+num4);
-		
-		
+		System.out.println(num3 + num4);
+
+		// 3. 브라우저창에 출력
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		// cal값에 따라 해당하는 연산의 값이 출력
+		if (cal.equals("+")) {
+			out.println(num3 + "+" + num4 + "=" + (num3 + num4));
+
+		} else if (cal.equals("-")) {
+			out.println(num3 + "+" + num4 + "=" + (num3 - num4));
+
+		} else if (cal.equals("*")) {
+			out.println(num3 + "+" + num4 + "=" + (num3 * num4));
+
+		} else if (cal.equals("/")) {
+			out.println(num3 + "+" + num4 + "=" + (num3 / num4));
+
+		}
+
 	}
 
 }
