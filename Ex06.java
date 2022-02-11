e204e8cb6ed646f7ed121196fdc9c31dc0e0feaa
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-02-12 00:43:48 +0900

    Update Ex06.java

--- Ex06.java
+++ Ex06.java
@@ -7,31 +7,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Ex06")
-public class Ex06 extends HttpServlet {
+@WebServlet("/ex06")
+public class ex06 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//post 방식이면 반드시 작성
-		request.setCharacterEncoding("utf-8");
-		
-		//hobby로 중복 선택된 값을 배열값으로 데이터 저장 -> getParameterValues
-		//getParameterValues은 배열 타입 -> String[]
-		//사용자로부터 받은 값을 배열 타입의 변수 hobby로 받음
-		String[] hobby = request.getParameterValues("hobby");
-		
-		//문자 타입의 hobby_res 변수는 비어있음
-		String hobby_res = "";
-		
-		//hobby의 크기만큼 i를 반복하면서 hobby[인덱스번호]에 해당하는 값을 hobby_res에 누적해서 추가
-		for (int i = 0; i < hobby.length; i++) {
-			hobby_res += hobby[i];
-		}
-		
-		String gender = request.getParameter("gender");
-		
-		System.out.println("선택한 취미 : " + hobby_res);
-		System.out.println("선택한 성별 : " + gender);
-	}
 
+      request.setCharacterEncoding("utf-8");
+      String id=request.getParameter("id");
+      String pw=request.getParameter("pw");
+      String gender=request.getParameter("gender");
+      String b=request.getParameter("b");
+      String birth=request.getParameter("birth");
+      
+      String[] hobby = request.getParameterValues("hobby");
+      String color=request.getParameter("color");
+      String talk=request.getParameter("talk");
+      
+      String hobby1="";
+      for (int i = 0; i < hobby.length; i++) {
+         hobby1=hobby1+hobby[i]+" ";
+      }
+      System.out.println("ID : "+id);
+      System.out.println("PW : "+pw);
+      System.out.println("gender : "+gender);
+      System.out.println("blood type : "+b);
+      System.out.println("birth : "+birth);
+      System.out.println("hobby : "+hobby1);
+      System.out.println("color : "+color);
+      System.out.println("talk : "+talk);
+      
+      response.setContentType("text/html;charset=utf-8");
+      PrintWriter out =response.getWriter(); 
+      
+      out.println("ID : "+id+"<br>");
+      out.println("PW : "+pw+"<br>");
+      out.println("gender : "+gender+"<br>");
+      out.println("blood type : "+b+"<br>");
+      out.println("birth : "+birth+"<br>");
+      out.println("hobby : "+hobby1+"<br>");
+      out.println("color : "+color+"<br>");
+      out.println("talk : "+talk+"<br>");
+   }
 }
