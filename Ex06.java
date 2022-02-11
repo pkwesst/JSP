5c31193e96b4008524ff9d509581e328ee5b5a9f
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-02-12 00:36:53 +0900

    Update Ex06.java

--- Ex06.java
+++ Ex06.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Ex05")
+@WebServlet("/Ex06")
 public class Ex06 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
