cd985971e2b9342700a126be578d5f4a679b1710
박서현 <90893579+pkwesst@users.noreply.github.com>
2022-02-14 00:25:56 +0900

    Update Ex06.java

--- Ex06.java
+++ Ex06.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,42 +12,68 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ex06")
 public class ex06 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-      request.setCharacterEncoding("utf-8");
-      String id=request.getParameter("id");
-      String pw=request.getParameter("pw");
-      String gender=request.getParameter("gender");
-      String b=request.getParameter("b");
-      String birth=request.getParameter("birth");
-      
-      String[] hobby = request.getParameterValues("hobby");
-      String color=request.getParameter("color");
-      String talk=request.getParameter("talk");
-      
-      String hobby1="";
-      for (int i = 0; i < hobby.length; i++) {
-         hobby1=hobby1+hobby[i]+" ";
-      }
-      System.out.println("ID : "+id);
-      System.out.println("PW : "+pw);
-      System.out.println("gender : "+gender);
-      System.out.println("blood type : "+b);
-      System.out.println("birth : "+birth);
-      System.out.println("hobby : "+hobby1);
-      System.out.println("color : "+color);
-      System.out.println("talk : "+talk);
-      
-      response.setContentType("text/html;charset=utf-8");
-      PrintWriter out =response.getWriter(); 
-      
-      out.println("ID : "+id+"<br>");
-      out.println("PW : "+pw+"<br>");
-      out.println("gender : "+gender+"<br>");
-      out.println("blood type : "+b+"<br>");
-      out.println("birth : "+birth+"<br>");
-      out.println("hobby : "+hobby1+"<br>");
-      out.println("color : "+color+"<br>");
-      out.println("talk : "+talk+"<br>");
-   }
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		//id
+		String id = request.getParameter("id");
+		System.out.println("id : " + id);
+		out.println("id : " + id);
+		out.println("<br>");
+		
+		//pw
+		String pw = request.getParameter("pw");
+		System.out.println("pw : " + pw);
+		out.println("pw : " + pw);
+		out.println("<br>");
+
+		//gender
+		String gender = request.getParameter("gender");
+		System.out.println("gender : " + gender);
+		out.println("gender : " + gender);
+		out.println("<br>");
+
+		//b 
+		String b = request.getParameter("b");
+		System.out.println("b : " + b);
+		out.println("b : " + b);
+		out.println("<br>");
+
+		//date
+		String date = request.getParameter("date");
+		System.out.println("date : " + date);
+		out.println("date : " + date);
+		out.println("<br>");
+
+		//hobby 중복체크
+		String[] hobby = request.getParameterValues("hobby");
+		String hobby_res = "";
+
+		for (int i = 0; i < hobby.length; i++) {
+			hobby_res += hobby[i];
+		}
+
+		System.out.println("hobby : " + hobby_res);
+		out.println("hobby : " + hobby_res);
+		out.println("<br>");
+
+		//color
+		String color = request.getParameter("color");
+		System.out.println("color : " + color);
+		out.println("color : " + color);
+		out.println("<br>");
+
+		//talk
+		String talk = request.getParameter("talk");
+		System.out.println("talk : " + talk);
+		out.println("talk : " + talk);
+		out.println("<br>");
+	}
+
 }
